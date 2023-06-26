a5f4b4851eef3356c97532bfb59c19fc9f95568b
Emaiya James-Plew <emaiya.jp@gmail.com>
2023-06-26 15:45:28 -0600

    Initial Commit

---
+++ whosThatDuck/whosThatDuckApp.swift
@@ -0,0 +1,17 @@
+//
+//  whosThatDuckApp.swift
+//  whosThatDuck
+//
+//  Created by Scholar on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct whosThatDuckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
