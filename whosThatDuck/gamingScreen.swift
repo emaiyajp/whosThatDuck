5f736ec78bc2d4813038c0275cc7e1a34dbc1d02
Emaiya James-Plew <emaiya.jp@gmail.com>
2023-06-27 15:41:43 -0600

    Added file

---
+++ whosThatDuck/gamingScreen.swift
@@ -0,0 +1,20 @@
+//
+//  gamingScreen.swift
+//  whosThatDuck
+//
+//  Created by Scholar on 6/27/23.
+//
+
+import SwiftUI
+
+struct gamingScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct gamingScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        gamingScreen()
+    }
+}
