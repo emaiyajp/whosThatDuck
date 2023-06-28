51a0fe50e3b57c817de9b5ae8b78d5d33ed684bf
Emaiya James-Plew <emaiya.jp@gmail.com>
2023-06-28 14:02:48 -0600

    Added to revealed answer

--- whosThatDuck/MallardAnswer.swift
+++ whosThatDuck/MallardAnswer.swift
@@ -9,12 +9,42 @@ import SwiftUI
 
 struct MallardAnswer: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack {
+            (Color(red: 0.027450980392156862, green: 0.5607843137254902, blue: 0.43137254901960786))
+                .ignoresSafeArea()
+            VStack {
+                
+                Text("Who's That Duck?")
+                    .font(.headline)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                Spacer()
+                    .padding()
+                
+                Image("Mallard")
+                    .resizable(resizingMode: .stretch)
+                    .aspectRatio(contentMode: .fill)
+                Spacer()
+                    .padding()
+                
+                Text("This is a...")
+                
+                Text("Mallard Duck")
+                
+                Text("[info and awareness]")
+                    .font(.headline)
+                    .foregroundColor(Color.white)
+                Spacer()
+                    .padding()
+            }
+        }
     }
-}
+        
+        struct MallardAnswer_Previews: PreviewProvider {
+            static var previews: some View {
+                MallardAnswer()
+            }
+        }
+    }
 
-struct MallardAnswer_Previews: PreviewProvider {
-    static var previews: some View {
-        MallardAnswer()
-    }
-}
